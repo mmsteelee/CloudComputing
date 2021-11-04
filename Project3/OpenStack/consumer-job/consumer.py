0cc75fc82b9e9f838ea580338e93b8d0c14fe12c
mmsteelee <matthew.r.steele@vanderbilt.edu>
2021-11-04 10:03:46 -0500

    Consumer code updated

--- Project3/OpenStack/consumer-job/consumer.py
+++ Project3/OpenStack/consumer-job/consumer.py
@@ -1,42 +1,33 @@
-import couchdb
 import json
-import time # for sleep
+import os   # need this for popen
 from kafka import KafkaConsumer  # consumer of events
+import couchdb
 
-# acquire the consumer
-consumer = KafkaConsumer (bootstrap_servers="129.114.26.148:30006",
-                                    value_deserializer=lambda m:
-                                    json.loads(m.decode('utf-8')))
+ip_kafka = "129.114.26.148"
+ip_couchdb = "129.114.27.112"
+consumer = KafkaConsumer (bootstrap_servers="{}:30000".format(ip_kafka), value_deserializer=lambda v: json.loads(v).encode('ascii'))
 
-# subscribe to topic
 consumer.subscribe (topics=["utilizations"])
+
+# acquire couchdb server
+user = "admin"
+password = "admin"
+conn_string  = "http://{}:{}@{}:30006".format(user, password, ip_couchdb)
+print(conn_string)
+couch = couchdb.Server(conn_string)
+dbname = "team10-data"
+
+if dbname in couch:
+    db = couch[dbname]
+else:
+    db = couch.create(dbname)
 
 # we keep reading and printing
 for msg in consumer:
-    # what we get is a record. From this record, we are interested in printing
-    # the contents of the value field. We are sure that we get only the
-    # utilizations topic because that is the only topic we subscribed to.
-    # Otherwise we will need to demultiplex the incoming data according to the
-    # topic coming in.
-
-    # placeholder code for M2
-    # print(msg.value)
-
-    # couchdb code for M3
-    couch = couchdb.Server("http://129.114.27.112:5984")
-
-    # username/password if necessary
-    server.resource.credentials = ("admin", "admin")
-
-    # create db
-    if "cdb-db" not in couch:
-         couch.create("cdb-db")
-    #
-    db = couch["cdb-db"]
-    db_entry = json.load(msg.value)
-    db.save(db_entry)
-
-# we are done. As such, we are not going to get here as the above loop
-# is a forever loop.
+    print (msg.value)
+    document = json.loads(msg.value)
+    #db.save(msg.value)
+    #db.save({'type': 'Person', 'name': 'John Doe'})
+    db.save(document)
 consumer.close ()
     
